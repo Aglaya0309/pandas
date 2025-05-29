4b5fc0853547dce0bd56a2f93e94876c5bdf7cbb
ElviraKondratova <elvirochka_sh@mail.ru>
2025-05-29 13:11:14 +0300

    done 1dz

---
+++ pandas 1dz.py
@@ -0,0 +1,12 @@
+#Загрузите набор данных из CSV-файла в DataFrame.
+# Выведите первые 5 строк данных, чтобы получить представление о структуре данных.
+# Выведите информацию о данных (.info()) и статистическое описание (.describe()).
+
+import pandas as pd
+
+df = pd.read_csv("binance-coin.csv")
+print(df.head())
+
+print (df.info())
+
+print(df.describe())
